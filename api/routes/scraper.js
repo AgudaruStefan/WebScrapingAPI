6ba33147f248656a4f067c0e6853412adb283b13
Aguda <agudarustefan@gmail.com>
2023-10-13 14:35:11 +0300

    Reformat with Prettier

--- api/routes/scraper.js
+++ api/routes/scraper.js
@@ -1,142 +1,150 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
+const puppeteer = require("puppeteer");
+const express = require("express");
 const router = express.Router();
 
-Array.prototype.slice_ = function(start,end,step=1) {
-    return this.slice(start,end)
-        .reduce((acc, e, i) => i % step == 0 
-        ? [...acc, e] 
-        : acc, []); 
-}
+Array.prototype.slice_ = function (start, end, step = 1) {
+  return this.slice(start, end).reduce(
+    (acc, e, i) => (i % step == 0 ? [...acc, e] : acc),
+    []
+  );
+};
 
 function countConsonants(word) {
-    const consonants = 'BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz';
-  
-    let consonantCount = 0;
-  
+  const consonants = "BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz";
 
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (consonants.includes(char)) {
-        consonantCount++;
-      }
+  let consonantCount = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    if (consonants.includes(char)) {
+      consonantCount++;
     }
-  
-    return consonantCount;
   }
+
+  return consonantCount;
+}
 
 function countVowels(word) {
-    const vowels = 'AEIOUaeiou';
+  const vowels = "AEIOUaeiou";
 
-    let vowelCount = 0;
+  let vowelCount = 0;
 
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (vowels.includes(char)) {
-        vowelCount++;
-      }
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    if (vowels.includes(char)) {
+      vowelCount++;
     }
-  
-    return vowelCount;
   }
 
-const avg = arr => {
-const sum = arr.reduce((acc, cur) => acc + cur);
-const average = sum/arr.length;
-return average;
+  return vowelCount;
 }
+
+const avg = (arr) => {
+  const sum = arr.reduce((acc, cur) => acc + cur);
+  const average = sum / arr.length;
+  return average;
+};
 
 async function scrape(URL) {
   try {
-    
     const browser = await puppeteer.launch('headless: "new"');
     const page = await browser.newPage();
-    await page.goto(URL, { waitUntil: 'domcontentloaded' });
+    await page.goto(URL, { waitUntil: "domcontentloaded" });
 
-
-    await page.waitForSelector('html');
+    await page.waitForSelector("html");
 
     const html = await page.content();
 
+    const TitleMatches = html.match(
+      /<a[^>]*><span[^>]*>(.*?)<\/span>(.*?)<\/a>/gs
+    );
+    const title = TitleMatches
+      ? TitleMatches.map((match) =>
+          match.replace(/<\/?span[^>]*>|<\/?a[^>]*>/g, "")
+        )
+      : [];
 
-    const TitleMatches = html.match(/<a[^>]*><span[^>]*>(.*?)<\/span>(.*?)<\/a>/gs);
-    const title = TitleMatches ? TitleMatches.map(match => match.replace(/<\/?span[^>]*>|<\/?a[^>]*>/g, ''))
-    : [];
-
-    const descriptions = await page.$$eval('div div div div div', div => div.map(div => div.textContent));
-    const descriptionslice = descriptions.slice_(9,-1,14);
+    const descriptions = await page.$$eval("div div div div div", (div) =>
+      div.map((div) => div.textContent)
+    );
+    const descriptionslice = descriptions.slice_(9, -1, 14);
 
     const imageMatches = html.match(/<img.*?src=["'](.*?)["']/g);
-    const images = imageMatches ? imageMatches.map(match => match.match(/src=["'](.*?)["']/)[1])
-    .filter(src => src.includes('w=3840&amp')): null;
+    const images = imageMatches
+      ? imageMatches
+          .map((match) => match.match(/src=["'](.*?)["']/)[1])
+          .filter((src) => src.includes("w=3840&amp"))
+      : null;
 
-    const HrefMatches = html.match(/<a[^>]*href="([^"]*)"><span[^>]*>(.*?)<\/span>/gs);
-    const hrefs = HrefMatches ? HrefMatches.map(match => match.match(/<a[^>]*href="([^"]*)"/)[1]): null;
-    
-    
+    const HrefMatches = html.match(
+      /<a[^>]*href="([^"]*)"><span[^>]*>(.*?)<\/span>/gs
+    );
+    const hrefs = HrefMatches
+      ? HrefMatches.map((match) => match.match(/<a[^>]*href="([^"]*)"/)[1])
+      : null;
+
     const wordCounts = [];
     const vowelss = [];
     const cons = [];
-    const conminvowelss = []
-    const sentiment = []
+    const conminvowelss = [];
+    const sentiment = [];
 
     for (const href of hrefs) {
-        const hrefPage = await browser.newPage();
-        await hrefPage.goto(URL + href, { waitUntil: 'domcontentloaded' });
-        await hrefPage.waitForSelector('html');
+      const hrefPage = await browser.newPage();
+      await hrefPage.goto(URL + href, { waitUntil: "domcontentloaded" });
+      await hrefPage.waitForSelector("html");
 
-        const hrefHtml = await hrefPage.content();
-        const textWithoutHtml = hrefHtml.replace(/<[^>]+>/g, '');
-        const words = textWithoutHtml.split(/\s+/);
-        const validWords = words.filter(word => word.trim() !== '');
-        const vowelCount = countVowels(validWords.join(''));
-        const consonantCount = countConsonants(validWords.join(''));
-        cons.push(consonantCount)
-        vowelss.push(vowelCount)
-        if (consonantCount > vowelCount){
-            conminvowelss.push(consonantCount - vowelCount)
-        }
-        wordCounts.push(validWords.length);
-    };
+      const hrefHtml = await hrefPage.content();
+      const textWithoutHtml = hrefHtml.replace(/<[^>]+>/g, "");
+      const words = textWithoutHtml.split(/\s+/);
+      const validWords = words.filter((word) => word.trim() !== "");
+      const vowelCount = countVowels(validWords.join(""));
+      const consonantCount = countConsonants(validWords.join(""));
+      cons.push(consonantCount);
+      vowelss.push(vowelCount);
+      if (consonantCount > vowelCount) {
+        conminvowelss.push(consonantCount - vowelCount);
+      }
+      wordCounts.push(validWords.length);
+    }
 
-    const sentimentAvg = avg(conminvowelss)
+    const sentimentAvg = avg(conminvowelss);
     for (let i = 0; i < conminvowelss.length; i++) {
-        if (conminvowelss[i] < sentimentAvg){
-            sentiment.push("negative")
-        } else if (conminvowelss[i] > sentimentAvg){
-            sentiment.push("positive")
-        } else if ( conminvowelss === sentimentAvg) {
-            sentiment.push("neutral")
-        }
-    };
-
+      if (conminvowelss[i] < sentimentAvg) {
+        sentiment.push("negative");
+      } else if (conminvowelss[i] > sentimentAvg) {
+        sentiment.push("positive");
+      } else if (conminvowelss === sentimentAvg) {
+        sentiment.push("neutral");
+      }
+    }
 
     const blogInfo = {
-        Posts: [],
-      };
-  
-      for (let i = 0; i < title.length; i++) {
-        blogInfo.Posts.push({
-          title: title[i],
-          short_description: descriptionslice[i],
-          image: URL + images[i],
-          href: URL + hrefs[i],
-          sentiment: sentiment[i],
-          Words: wordCounts[i],
-        });
-      }
+      Posts: [],
+    };
+
+    for (let i = 0; i < title.length; i++) {
+      blogInfo.Posts.push({
+        title: title[i],
+        short_description: descriptionslice[i],
+        image: URL + images[i],
+        href: URL + hrefs[i],
+        sentiment: sentiment[i],
+        Words: wordCounts[i],
+      });
+    }
 
     await browser.close();
-    return(blogInfo);
+    return blogInfo;
   } catch (error) {
     console.error(error);
   }
 }
 
-router.get('/:url', async (req, res, next) => {
+router.get("/:url", async (req, res, next) => {
   const url = req.params.url;
   res.status(200).json({
-      WebScrapingAPI: await scrape(`https://${url}`)
+    WebScrapingAPI: await scrape(`https://${url}`),
   });
 });
 
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const scraper = require('./api/routes/scraper');
+const scraper = require("./api/routes/scraper");
 
-app.use('/scraper', scraper);
+app.use("/scraper", scraper);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
